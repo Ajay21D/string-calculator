99b22892104f48a890c0c9ab6d3ee3b21d26d1f1
Ajay <ajay.diware@humancloud.co.in>
2025-01-17 17:40:32 +0530

    output getting 0 for integer paramenter

---
+++ tddAssessment/src/main/java/org/example/StringCal.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+public class StringCal {
+
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+return 0;
+    }
+
+
+    public static void main(String[] args) {
+        StringCal calculator = new StringCal();
+
+        System.out.println(" : TDD Assessment :");
+        System.out.println("OutPut are as Follow :");
+        System.out.println(calculator.add("2"));
+
+    }
+}
+
+
